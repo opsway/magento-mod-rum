6c049d88580076fc50e558e025d0db8b063a45b2
Andriy Samilyak <samilyak@gmail.com>
2013-12-27 23:49:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+=== Magento module for real-user monitoring using GoogleAnalytics ===
+
+=== Usage ==
+
+1. Install to Magento by copying to directory structure
+2. Check that in System->Configuration->Web->Cookie domain you have specified your domain
---
+++ app/code/community/OpsWay/RUM/Block/Boomerang.php
@@ -0,0 +1,29 @@
+<?php
+
+class OpsWay_RUM_Block_Boomerang extends Mage_Core_Block_Template
+{
+    public function getPageType()
+    {
+        $handles = $this->getLayout()->getUpdate()->getHandles();
+        if (in_array("cms_page", $handles) 
+        	&& stristr(Mage::getSingleton('cms/page')->getIdentifier(),"home")) {
+        	$pagetype = "Home page";
+        } elseif (in_array("catalog_product_view", $handles)) {
+        	$pagetype = "Product page";
+        } elseif (in_array("catalog_category_view", $handles)) {
+        	$pagetype = "Category page";
+        } elseif (in_array("checkout_cart_index", $handles)) {
+        	$pagetype = "Shopping cart";
+        } elseif (in_array("checkout_onepage_index", $handles)) {
+        	$pagetype = "Onepage checkout";
+        } elseif (in_array("customer_account_login", $handles)) {
+        	$pagetype = "Account login page";
+        } elseif (in_array("customer_account_index", $handles)) {
+        	$pagetype = "My account";
+        } else {
+			$pagetype = "Other";        	
+        }
+
+        return $pagetype;
+    }
+}
