cea99180e6e72ea1f8d4e351001469eef4fc3a69
Andriy Samilyak <samilyak@gmail.com>
2013-12-29 14:12:23 +0200

    CMS pages handling

--- app/code/community/OpsWay/RUM/Block/Boomerang.php
+++ app/code/community/OpsWay/RUM/Block/Boomerang.php
@@ -6,27 +6,27 @@ class OpsWay_RUM_Block_Boomerang extends Mage_Core_Block_Template
 
     public function getPageType()
     {
-
         if (!$this->pagetype) {
             $handles = $this->getLayout()->getUpdate()->getHandles();
-            # Home page can be found also by  stristr(Mage::getSingleton('cms/page')->getIdentifier(),"home")
             if (in_array("cms_page", $handles) 
-                && Mage::getSingleton('cms/page')->getIdentifier() == "home") {
-                $pagetype = "Home page";
+                && stristr(Mage::getSingleton('cms/page')->getIdentifier(),"home")) {
+                $this->pagetype = "Home page";
+            } elseif (in_array("cms_page", $handles)) {
+                $this->pagetype = "CMS page";
             } elseif (in_array("catalog_product_view", $handles)) {
-                $pagetype = "Product page";
+                $this->pagetype = "Product page";
             } elseif (in_array("catalog_category_view", $handles)) {
-                $pagetype = "Category page";
+                $this->pagetype = "Category page";
             } elseif (in_array("checkout_cart_index", $handles)) {
-                $pagetype = "Shopping cart";
+                $this->pagetype = "Shopping cart";
             } elseif (in_array("checkout_onepage_index", $handles)) {
-                $pagetype = "Onepage checkout";
+                $this->pagetype = "Onepage checkout";
             } elseif (in_array("customer_account_login", $handles)) {
-                $pagetype = "Account login page";
+                $this->pagetype = "Account login page";
             } elseif (in_array("customer_account_index", $handles)) {
-                $pagetype = "My account";
+                $this->pagetype = "My account";
             } else {
-            $pagetype = "Other";            
+                $this->pagetype = "Other";            
             }
         }
 
